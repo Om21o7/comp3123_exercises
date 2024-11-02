82e90fbd94b8d0bc0e1836bc9bfab60fb0969626
ommakwana <101414422@georgebrown.ca>
2024-11-02 15:22:57 -0400

    Lab Exercise07 added

---
+++ 101414422_COMP3123_exec07/labweek09/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to FullStack Development - I</h1>
+        <h>React JS Programming Week09 Lab exercise</h>
+        <h3>Student ID : 101414422</h3>
+        <h3>Student Name: Om Makwana</h3>
+        <h3>George Brown College, Toronto</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
